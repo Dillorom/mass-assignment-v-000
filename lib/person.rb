d45076d59555ba741b633ba2d6900953d5b89e82
Dillorom Abdulloeva <dabdulloeva@gmail.com>
2018-05-17 19:49:22 +0000

    automatically backed up by learn

--- lib/person.rb
+++ lib/person.rb
@@ -1,3 +1,10 @@
 class Person
-  #your code here
+  attr_accessor :name, :birthday, :hair_color, :eye_color, :height
+
+  def initialize(name:, birthday:, hair_color:, eye_color:, height:)
+    @name = name
+    @birthday = birthday
+    @hair_color = hair_color
+    @eye_color = eye_color
+    @height = height
 end
